5b58727e6a69116df7daef8a00b8676e984d862b
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-01 17:22:30 +0530

    Create introduction.py
    intro

---
+++ introduction.py
@@ -0,0 +1,6 @@
+Its a high level langauage.
+Known for simple and redable syntax
+Allow writing clean code with fewer lines
+Supports object oriented, functional and procedural programming langauge.
+Widly used in web development, automation and ai and data analysis and many other fields
+Dynamic typed and has auto garbage collection
