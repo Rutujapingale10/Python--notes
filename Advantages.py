dd26dd7da6cd8f33dce259eaace76040b01cfb1b
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-01 18:49:48 +0530

    Create Advantages.py
    python application and advantages

---
+++ Advantages.py
@@ -0,0 +1,21 @@
+Python is widly used across indusries. It power application in web development, data science, machine learning, automation scientific computating and many other domain.
+applications - youtube,instagaram,netflix,uber etc
+
+libraries used - 
+Web devlopment framework likes - django, flask
+Data science and analysis - numpy and panda, matplotlib
+machine learning and ai  - tensorflow,pytorch
+Automation and scripting - automate repetative task
+Game development library - Pygame
+Web scrapping tool - Beautifulsoup ,scrapy
+Scintific computing - scipy
+nternate of things - Macro python ras berry pi
+devops and cloud  - Automation and scripts
+
+
+Advantages - 
+Presence of third party modules - Python has rich echosystem of third party modules and libraries 
+extensive support libraries - Python boasts extensive support libraries like numpy libraries for numerical calculations and pandas for data analytics making it suitable for scientific and data related application.
+Open source and large active community - python is a open source and it has large and active community that contribute to its development and provide support.
+Dynamically types langauage  - Pyhton is a dynamically types means no need to declared data type explicitly, making it flexible but still relable.
+Object oriented and procedurl programming language  - python supports both OOP and procedural programming language 
