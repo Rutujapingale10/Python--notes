feeeea536726b8ea3b7eaa29cd5a312a45a20850
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-02 17:11:30 +0530

    Create 3python_operator.py
    operators in python

---
+++ 3python_operator.py
@@ -0,0 +1,45 @@
+In python programming, Operators in general are used to perform operations on values and variables.
+Operator symbol like - +,-*,/
+operand - Value on which the operator is applied 
+
+In python arithmatic operator can used division operator / return floating pont result while floor division operator // 
+return integer result 
+
+
+#comparison operator - 
+a = 13
+b= 33
+print(a > b)
+print(a < b)
+print(a == b)
+print( a!=b )
+print( a >= b)
+print( a<= b)
+
+#logical operator 
+Logical not 
+Logical or
+Logical and
+
+a = True
+b= False
+print(a and b)
+print(a or b)
+print(not a)
+
+#assignment operator 
+ a= 10
+b = 20
+print(b)
+b = b+a
+print(b)
+b= b-a
+print(b)
+b = b*a
+print(b)
+
+
+Ternary operator its a shorthand for if else
+a, b= 10,20
+ min = a if a<b else b
+
