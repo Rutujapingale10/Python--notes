44a80ab4dbb501cca06745d73d6eb86c9e495ab9
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-02 19:00:36 +0530

    Create 4conditional_statement.py

---
+++ 4conditional_statement.py
@@ -0,0 +1,43 @@
+Conditional statement are used to control flow of program based on some condition
+
+if statement - if statement is used to execute a block of code only when specified conditon used to evalute to true.
+
+if age >= 20
+  print("eligible to vote")
+
+age = 19
+if age > 18:print("eligible to vote")  ---> short hand for if else
+
+if else statement
+
+  age =10
+if age <= 12
+     print("travel for free")
+else:
+     print("pay for ticket")
+
+if elif else statement
+
+
+age = 35
+     if age <=12
+       print("child")
+     elif age <=19
+     print("teenager")
+     elif age <= 35
+     print("young adult")
+     else
+     print("adult")
+
+
+     Match case statement
+     number =2 
+     match number
+     case 1:
+        print("one")
+     case 2:
+         print("two")
+     case_:
+         print("other number")
+     
+     
