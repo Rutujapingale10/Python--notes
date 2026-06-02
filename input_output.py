c50b23ce45e071e504221edf14ed1bbfb98087f0
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-02 13:44:45 +0530

    Create input_output.py
    input output of python

---
+++ input_output.py
@@ -0,0 +1,24 @@
+Input and Output in Python 
+output() - python output function is used to print output
+
+pythons input() function is used to take user input by default it return input in string format
+
+eg 
+name = input("Enter your name")
+print("Hello",name,"Welcome!")
+
+Printing variables
+s = "brad"
+print(s)
+
+s = "anjelina"
+print(s)
+age = 25
+city = "Pune"
+print(age,city)
+
+#multi input at a time'
+we can take multiple input at a time 
+by using input() method only and can split it later by using .split()
+
+x,y = input("Enter two values:").split()
