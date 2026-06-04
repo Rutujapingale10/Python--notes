d21dccfa6373ead1f52f7c7022a1559bd981b2a6
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-04 14:58:11 +0530

    Create 4venv.py
    virtual env

---
+++ 4venv.py
@@ -0,0 +1,16 @@
+A virtual enviornment in python is isolated env in your computer where you can run and test your python 
+It allow us to manage project specific dependancy
+
+Virtual env can act as seperate container for each python project. Each container has - 
+  - Its own python interpreter.
+  - Has its own set of installed packages 
+  - Ia isolated from other virtual enviornment
+ - can have different version of same packages 
+
+
+- Using virtual env is important because
+  it prevents package version conflicts between projects
+  make project more portable and reproducible.
+keep your system python installation clean 
+    Allows testing with different python version.
+    
