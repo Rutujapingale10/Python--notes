8acc2bcf9546f909264046fe2981c7f23942f63e
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-04 16:04:52 +0530

    Update 5OOP.py

--- 5OOP.py
+++ 5OOP.py
@@ -1,1 +1,26 @@
+Python is a object oriented programming language, allowing you to structure your code using classes and objects for better organisation and reusability
+
+Advantages of OOP - 
+- provides a clear structure to a program 
+- Makes code easier to maintain and debug
+- Allow you to build resusable applications with less code
+
+  Object - 
+  In python everything is a object with its properties and method.
+  Object is a something which has state behaviour and responsibilities.
+
+  class - class is a blueprit for real world entity.
+
+
+  class Myclass:
+    x = 5 ------->creating class
+
+  p1 = MyClass()
+  print(p1.x)
+
+
+#deleting object 
+del p1
+
+
 
