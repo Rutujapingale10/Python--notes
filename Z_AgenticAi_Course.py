30260af09b92e020d71ee51770d617783e14b578
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 11:12:19 +0530

    Create Z_AgenticAi_Course.py

---
+++ Z_AgenticAi_Course.py
@@ -0,0 +1,13 @@
+Level	Title	Link	Duration in hrs	Mandate
+Basic	Generative AI Fundamentals	https://www.udacity.com/org/accenture-all-access/course/generative-ai-fundamentals--cd13303	21	Optional
+Basic	AI Programming with Python	Agentic Workflows with Microsoft Azure | Udacity Course | Udacity	52	Optional
+Intermediate	AI Agents with LangChain and LangGraph	https://www.udacity.com/org/accenture-all-access/course/ai-agents-with-langchain-and-langgraph--cd1…	15	Mandatory
+Intermediate	Building Agents	https://www.udacity.com/org/accenture-all-access/course/building-agents--cd14524	11	Mandatory
+Intermediate	Model Context Protocol (MCP)	https://www.udacity.com/enrollment/cd14712	13	Mandatory
+Intermediate	Multi-Agent Systems	https://www.udacity.com/org/accenture-all-access/course/multi-agent-systems--cd14497	14	Mandatory
+Intermediate	Agentic AI Workflows with Microsoft Azure	Agentic Workflows with Microsoft Azure | Udacity Course | Udacity	15	Optional
+Intermediate	Agentic Workflows	https://www.udacity.com/org/accenture-all-access/course/agentic-workflows--cd14525	14	Mandatory
+Intermediate	Multimodal AI Applications	https://www.udacity.com/org/accenture-all-access/course/multimodal-ai-applications--cd13331	18	Mandatory
+Intermediate	Multi-Agent AI Systems with Microsoft Azure	https://www.udacity.com/org/accenture-all-access/course/multi-agent-ai-systems-with-microsoft-azure…	15	Mandatory
+Intermediate	Building a Custom OpenAI Chatbot	https://www.udacity.com/org/accenture-all-access/course/build-a-custom-chatbot-with-openAI-api--cd1…	6	Optional
+Advanced	AI Engineer using Microsoft Azure	Agentic Workflows with Microsoft Azure | Udacity Course | Udacity	54	Optional
