50dc71b0728ff06890054c50301e90ef530bf885
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 10:48:17 +0530

    Update 7constructor.py

--- 7constructor.py
+++ 7constructor.py
@@ -16,3 +16,25 @@ __init__ - method
 __init__  is a special method known as initialiser or constructor. It is called automatically when new instance of the class is created.
 
 
+	There are some types of constructor-
+	
+	Default constructor - constructor doesn’t take any parameter except self . It is used to initialize object using default values.
+	
+		Def __init__(self):
+			Self.geek = "Geeksforgeeks"
+			
+	Parameterized constructor - This constructor accepts argument from user and initializes
+	The object with those values.
+		Def __init__(self,f,s):
+		 
+		
+	__new__ Method - 
+		This method is responsible creating new instance of a class. It allocates memory and return new object. It is called before init.
+		
+	__init__ Method - 
+		This method initializes newly created instance and commonly used as constructor in python.
+		It is called immediately object created by __new__ method. This method responsible for initializing attributes of instance.
+		 
+
+
+
