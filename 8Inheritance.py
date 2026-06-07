61d1d1aa090c0cdca83e950a04c696b553cf7246
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 11:00:08 +0530

    Create 8Inheritance.py

---
+++ 8Inheritance.py
@@ -0,0 +1,16 @@
+Reuse of already defined classes with is a relationship is nothing but inheritance.
+It is used to reduce redudancy . Ie repeatation of value for same entity.
+
+Types 
+-single inheritance - 
+There is one base class and one derived class. Derived class is derived from its base class.
+
+-multiple inheritance -
+There are two or more base classes and child class is derived from more than one base class
+-multlevel inheritance
+Derived class is derived from its base class
+-hybrid inheritance
+Multiple classes inherit from single base class
+-hierachical inheritance
+
+Combination of two or more inheritance
