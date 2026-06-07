fb7067a701e8a5f02477e72c1db3b04ed5ac5157
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 11:03:47 +0530

    Create 12Encapsulation.py

---
+++ 12Encapsulation.py
@@ -0,0 +1,25 @@
+Encapsulation -
+It is a process of binding and by default hiding state and behavior.
+Encapsulation is nothing but writing classes providing access specifier to it and wraping data under single unit called object.
+
+Example below show 
+
+Class emp:
+	Def __init__(self,name,salary):
+	Self.name = name
+	Self.__salary = salary
+	
+	
+	Access specifier - 
+	Public - public access specifier are accessible anywhere 
+	
+	Private - private access specifier are accessible in its base class only 
+		Want to access outside class - use setter and getter using (.__) we can define them
+		
+	Protected access specifier - 
+		Protected access specifier are accessible in its base class also in derived class
+		They are indicated by (._) 
+	
+
+
+ 
