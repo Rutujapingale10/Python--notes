e9558333bef5df48d614160e258a8039a42ce567
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 10:47:08 +0530

    Create 7constructor.py
    Constructor in python difference between __init__ and __new__

---
+++ 7constructor.py
@@ -0,0 +1,18 @@
+Constructor - 
+Constructor is a special type of function which gets called automatically at the time of object creation.
+For every object constructor gets called seperatly and only once.
+
+The method __new__ is a constructor in class. It create new instance of class while
+__init__ is a initializer set up instance attribute after creation.
+These methods work together and setup object creation and initialisation.
+
+difference between init and new
+__new__ : method 
+__new__ in python is a special method thats responsible for actually creating  a new instance of class.
+
+  Its a step where object come into existance, even before it gets initialised with specific value.it gets called before __init__.
+
+__init__ - method 
+__init__  is a special method known as initialiser or constructor. It is called automatically when new instance of the class is created.
+
+
