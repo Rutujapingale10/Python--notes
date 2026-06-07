e1be5d4727cd4fb32ce2aefcbe3e45773fef2d72
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 11:05:29 +0530

    Create 13Dictionary.py

---
+++ 13Dictionary.py
@@ -0,0 +1,12 @@
+Dictonary - 
+It is key value combination where key should be unique .
+It is mutable in nature (changeable)
+It is unordered
+
+
+#Dictonary Method
+--mydic.keys()
+Mydict.update()
+Mydict.items() //return all(key,vakue) pair as tuple
+Mydict.get("Key") //returns key according to value
+Mydict.update()
