595b10c45123172b5beda944b0310733c15c0f62
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 11:08:58 +0530

    Create 17Function.py

---
+++ 17Function.py
@@ -0,0 +1,27 @@
+In python we can define function using def keyword. We can add any type of functionalities and properties to it as we require.
+
+
+The def keyword stands for define. It is used to create user define function.
+
+It marks the beginning of function block and allow you to group set of statements so they can be reuse when function is called.
+
+
+Default Argument :
+	Default argument is parameter that assumes a default value is not provided function call for that argument
+	
+	Types for functions:
+	Without return type without parameter
+	Without return type with parameter
+	With return type without parameter
+	With return type with parameter
+	
+Lambda function :
+Its a anynamous function it is function without name 
+
+In lambda function lambda keyword is must
+
+Pass Statement  - 
+Pass Statement in Python is aplaceholder which does nothing when executed its just a placeholder .
+Where statement is required but no logic needed such as empty function classes loop or conditional block.
+
+Without pass leaving block empty cause error 
