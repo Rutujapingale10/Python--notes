d244e9422bbeb16eb4991cb299dfc47d8479e90a
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2026-06-07 10:05:56 +0530

    Create 6class.py
    class

---
+++ 6class.py
@@ -0,0 +1,15 @@
+calss - class is user defined template for creating object.
+It bundles data and function together so that it can bundles and function together, making it easier to manage and  use them.
+
+Classes are created using keyword class followed by class name and a colon.
+Atributes are variable defined inside class and represent properties of class. Atributes defined using class operator.
+
+
+#define class
+class Dog:
+  sound = "bark"
+
+Object - 
+An object is a specific instance of class.
+It hold its own set of data (instance variable) and invoked method dr=efined by class. Multiple object can be created from same class , each with its unique atributes.
+
